2691ff58c837cf3736c9fb462e5cb4e041b3870d
Ayeshamjad <ayeshaamjad016@gmail.com>
2026-02-07 14:27:18 +0500

    Remove timestamp from output directory structure

--- utils/logger.py
+++ utils/logger.py
@@ -4,9 +4,9 @@ import os
 
 """ prepare logdir for tensorboard and logging output"""
 def set_log(output_dir, cfg_file, log_name):
-    t = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+    # t = time.strftime("%Y%m%d_%H%M%S", time.localtime())  # Timestamp disabled
     base_name = os.path.basename(cfg_file).split('.')[0]
-    log_dir = os.path.join(output_dir, base_name, log_name + "-" + t)
+    log_dir = os.path.join(output_dir, base_name, log_name)  # No timestamp
     logs = {}
     for temp in ['tboard', 'model', 'sample']:
         temp_dir = os.path.join(log_dir, temp)
